e1a8ea56811b5649d94187fe8898f73d4658cd55
snowlt23 <shsnow23@gmail.com>
2017-09-04 16:15:37 +0900

    add is_prime

--- pp_debug.cpp
+++ pp_debug.cpp
@@ -3,7 +3,7 @@
 #ifdef DEBUG
 #define debug_echo(e) cout << "L" << __LINE__ << ": " << e << endl
 #define debug_var(...) cout << "L" << __LINE__ << ": " pp_foreach(debug_var_elem, __VA_ARGS__) << endl
-#define debug_time2(i, e) auto start ## i = clock(); e; auto end ## i = clock(); cout << (double)(end ## i - start ## i) << "ms" << endl;
+#define debug_time2(i, e) auto start ## i = clock(); e; auto end ## i = clock(); cout << "L" << __LINE__ << ": " << (double)(end ## i - start ## i) << "ms" << endl;
 #define debug_time1(f, i, e) f(i, e)
 #define debug_time(e) debug_time1(debug_time2, __COUNTER__, e)
 #else
--- tp_math.cpp
+++ tp_math.cpp
@@ -1,6 +1,7 @@
 
 ll gcd(ll a,ll b){while(b){ll tmp = a % b;a = b;b = tmp;}return a;}
 ll lcm(ll a,ll b){return a / gcd(a,b) * b;}
+bool is_prime(ll n){switch(n){case 0:case 1: return false;case 2: return true;}if(n % 2 == 0) return false;for(ll i=3;i * i <= n; i += 2)if(n%i == 0) return false;return true;}
 
 #define def_varargs(name, f) \
   template<typename First> \
